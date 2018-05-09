76dba39b124289390c6aa895346804dd8431a8c6
Andrew Mockett <califormula@gmail.com>
2018-05-09 09:07:02 -0600

    setup instance_of test, passing

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,14 @@
 # YOUR TESTS GO IN HERE!
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/translator"
+
+class TranslatorTest < Minitest::Test
+  def test_instance_of
+    translator = Translator.new
+    
+    assert_instance_of Translator, translator
+  end
+
+  
+end
